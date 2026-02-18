980734b821af50c879ad6b363663c51365d1f916
veeta03 <veetareddypodduturu@gmail.com>
2026-02-19 01:05:43 +0530

    Initial commit

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-DATABASE_URL = "mysql+pymysql://root:@localhost/talentiq_db"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 
 engine = create_engine(DATABASE_URL)
